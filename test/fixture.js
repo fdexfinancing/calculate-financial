a4e357ac11909b30b685853df7e0d157d87ac1f4
Murillo <mmbfreitas@hotmail.com>
2017-06-10 17:19:27 -0300

    adjust git ignore

---
+++ test/fixture.js
@@ -0,0 +1,43 @@
+const balanceData = {
+    gross_revenue: 70855981,
+    net_income_year_before: 88670000,
+    net_income: 54650123,
+    sold_product_cost: 49450123,
+    adm_cost: 5977920,
+    sell_team_cost: 3031763,
+    op_cost: -1482630,
+    depreciation: 1185017,
+    financial_result: -6128342,
+    rev_exp_no_rec: 0,
+    other: 0,
+    exp_ir_csll: 0,
+    current_assets: 20724088,
+    cash_availability: 1739910,
+    customer_receive_year_before: 13740000,
+    customer_receive: 4368477,
+    stock_year_before: 11752000,
+    stock: 6602469,
+    no_current_assets: 49986018,
+    bills_pay_before: 26095000,
+    bills_pay: 16423411,
+    taxes_cp: 5157445,
+    taxes_lp: 5887392,
+    liabilities_cp: 31079203,
+    liabilities_lp: 34237169,
+    onerous_liability_cp: 9498347,
+    onerous_liability_lp: 9842582,
+    liquid_assets: 5393734,
+    leverage_quotient: 0.5,
+    adjust_liquid_debit: "",
+    target_value: 3000000,
+    cdi: 12.13,
+    target_term: 12,
+    month_quantity: 12,
+    dollar_revenue: "",
+    serasa: "",
+    refin: ""
+};
+
+module.exports = {
+    balanceData
+};
